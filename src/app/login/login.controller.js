317a646a9feaab09392ddb6370482426df33d339
sookinoby <sookinoby@gmail.com>
2016-06-01 00:31:32 -0700

    new flow intergration for grade k and grade 3

---
+++ src/app/login/login.controller.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('r2mdemo')
+    .controller('LoginController', LoginController);
+
+  /** @ngInject */
+  function LoginController(localStorageService,$log,$state,authService,SweetAlert) {
+    var vm = this;
+    vm.button_message = "Sign In";
+    vm.loggining = false;
+    vm.loginData = {};
+
+    vm.login = function() {
+        vm.loggining = true;
+        vm.button_message = "Signing In";
+        vm.loginData.userName = vm.username;
+        vm.loginData.password = vm.password;
+        console.log("Login clicked");
+
+        authService.login(vm.loginData)
+          .then(function(response) {
+            console.log("logging in the users")
+            vm.loginData.userName = "";
+            vm.loginData.password = "";
+            vm.loggining = false;
+            vm.button_message = "Sign In";
+            $log.debug(response);
+            $state.go('main');
+          })
+          .catch(function(response) {
+            vm.loggining = false;
+            vm.button_message = "Sign In";
+            var error_description = "Something went wrong, Please Try again later";
+            if(response !=null && response.error_description != null )
+            {
+              error_description = response.error_description;
+            }
+           else
+            {
+              error_description = "Something went wrong, Please Try again later";
+            }
+            SweetAlert.swal("",error_description,"error");
+            $log.debug(response);
+          });
+      };
+
+
+
+
+  }
+})();
